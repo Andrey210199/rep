d3205c20060717ab581c2a6967a9c5c55c776927
Andrey210199 <dtyaew@mail.ru>
2021-08-23 17:14:45 +0300

    fix

--- task4/Program.cs
+++ task4/Program.cs
@@ -10,69 +10,74 @@ namespace task4
     {
         static void Main(string[] args)
         {
-            StreamReader sr = new StreamReader(Console.ReadLine(), System.Text.Encoding.Default);
+             StreamReader sr = new StreamReader(Console.ReadLine(), System.Text.Encoding.Default);
 
-                var failread = new List<string>();
-                //читает файл
-                while (!sr.EndOfStream)
+                  var failread = new List<string>();
+                  //читает файл
+                  while (!sr.EndOfStream)
+                  {
+                      string line = sr.ReadLine();
+                      failread.Add(line);
+
+                  }
+
+                  int[] mass = new int[failread.Count];
+
+                  //делает массив из списка
+                  for (int i = 0; i < failread.Count; i++)
+                  {
+                      mass[i] = Convert.ToInt32(failread[i]);
+                  }
+
+            int[] b = new int[mass.Length]; //Копия массива
+            Array.Copy(mass, b, mass.Length);
+
+                int max = mass.Max();
+                int min = mass.Min();
+                int j = 0;
+            int otvet = 0;
+            int prover = 0;
+
+            for (int i = min; i< max; i++ )
+            {
+                while(j==0)
                 {
-                    string line = sr.ReadLine();
-                    failread.Add(line);
-
-                }
-
-                int[] mass = new int[failread.Count];
-
-                //делает массив из списка
-                for (int i = 0; i < failread.Count; i++)
-                {
-                    mass[i] = Convert.ToInt32(failread[i]);
-                }
-                double srednee;
-                int max = 0;
-                int min = 0;
-
-                //среднее значение
-                srednee = Math.Round(mass.Average());
-
-                //подшитывает количество операций
-                while (min != 1)
-                {
-                    if (mass.Max() < srednee)
+                    for (int k = 0; k < b.Length; k++)
                     {
-                        Array.Sort(mass);
-                        mass[mass.Length - 1] += 1;
-                        max += 1;
-
+                        if (b[k] < i)
+                        {
+                            b[k]++;
+                            prover++;
+                        }
+                        else if(b[k]>i)
+                        {
+                            b[k]--;
+                            prover++;
+                        }
+                        
                     }
-                    else if (mass.Min() < srednee)
-                    {
-                        Array.Sort(mass);
-                        mass[0] += 1;
-                        max += 1;
-                    }
-                    else if (mass.Min() > srednee)
-                    {
-                        Array.Sort(mass);
-                        mass[0] -= 1;
-                        max += 1;
-                    }
-                    else if (mass.Max() > srednee)
-                    {
-                        Array.Sort(mass);
-                        mass[mass.Length - 1] -= 1;
-                        max += 1;
-                    }
-
                     //Проверяет одинаковы ли перевый и последний элемент
-                    Array.Sort(mass);
-                    if (mass[0] == mass[mass.Length - 1])
+                    Array.Sort(b);
+                    if (b[0] == b[b.Length - 1])
                     {
-                        min = 1;
+                        j = 1;
+                        
                     }
+                   
+                }
+                
+                if (prover < otvet || otvet == 0)
+                {
+                    otvet = prover;
                 }
 
-                Console.WriteLine(max);
+               Array.Copy(mass, b, mass.Length); //Делает копию изначального массива
+                prover = 0;
+                j = 0;
+
+            }
+
+                Console.WriteLine(otvet);
             }
 
     }
